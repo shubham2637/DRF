3e56314b37f0bf057c392d6755041d291b3422a8
Shubham Singh <shubham37@outlook.com>
2023-05-16 23:39:25 +0530

    added movie_details api

--- watchmate/watchlist/views.py
+++ watchmate/watchlist/views.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from watchlist.models import Movie
 from django.http import JsonResponse
+
 
 # Create your views here.
 
@@ -8,7 +10,19 @@ from django.http import JsonResponse
 def movie_list(request):
     movies = Movie.objects.all()
     data = {
-        'movies' : list(movies.values())
+        'movies': list( movies.values() )
     }
+    return JsonResponse( data )
+
+
+def movie_details(request, movie_id):
+    data = dict()
+    try:
+        movie = Movie.objects.get( pk=movie_id )
+        data['name'] = movie.name
+        data['description'] = movie.description
+        data['active'] = movie.active
+    except ObjectDoesNotExist:
+        data['error'] = "Invalid Movie id"
     return JsonResponse(data)
 
