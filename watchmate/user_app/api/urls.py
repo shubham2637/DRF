514bb0f1468a15a0c7b2313143ec7a323af6bd5d
Shubham Singh <shubham37@outlook.com>
2023-05-18 02:29:33 +0530

    JWT token basic implementation

--- watchmate/user_app/api/urls.py
+++ watchmate/user_app/api/urls.py
@@ -1,10 +1,15 @@
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from user_app.api.views import registration_view, logout_view
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', registration_view, name='register'),
     path('logout_view/', logout_view, name='logout_view'),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
--- watchmate/watchmate/settings.py
+++ watchmate/watchmate/settings.py
@@ -103,7 +103,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
