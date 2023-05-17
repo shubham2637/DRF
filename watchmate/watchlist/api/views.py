4786cd359bfa8c104dc97f9f0e139fb7a7db3b58
Shubham Singh <shubham37@outlook.com>
2023-05-17 09:53:29 +0530

    GET and PUT object level serializer implemented

--- watchmate/watchlist/api/views.py
+++ watchmate/watchlist/api/views.py
@@ -6,20 +6,42 @@ from watchlist.models import Movie
 from watchlist.api.serializers import MovieSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, movie_id):
-    data = dict()
     try:
         movie = Movie.objects.get( pk=movie_id )
-        serializer = MovieSerializer(movie)
-        return Response(serializer.data)
+
     except ObjectDoesNotExist:
-        data['error'] = "Invalid Movie id"
-        return Response(data)
+        data = {"error": "Invalid Movie id"}
+        return Response( data )
+
+    if request.method == 'GET':
+        serializer = MovieSerializer( movie )
+        return Response( serializer.data )
+
+    if request.method == 'PUT':
+        serializer = MovieSerializer(movie, data=request.data )
+        if serializer.is_valid():
+            serializer.save()
+            return Response( serializer.data )
+        else:
+            return Response( serializer.errors )
+
+
+
