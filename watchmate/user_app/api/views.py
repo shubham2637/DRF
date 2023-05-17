93d8a2e5c03d70742f699914144c49ee0ba3fc7f
Shubham Singh <shubham37@outlook.com>
2023-05-18 02:40:43 +0530

    JWT token basic implementation completed

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -4,7 +4,19 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 
 from user_app.api.serializers import RegistrationSerializer
-from user_app import models
+# from user_app import models
+
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 
 @api_view( ['POST', ] )
@@ -16,9 +28,8 @@ def registration_view(request):
             account = serializer.save()
             data['username'] = account.username
             data['email'] = account.email
-
-            token = Token.objects.get( user_id=account.pk ).key
-            data['token'] = token
+            refresh = get_tokens_for_user(user=account)
+            data['token'] = refresh
             data['res_str'] = "Registration Successful"
             return Response( data, status=HTTP_201_CREATED )
         data = serializer.errors
